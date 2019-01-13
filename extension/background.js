d3f65de28671644f371238564e82e34deeebf5e9
Tarik <tarikh@uw.edu>
2019-01-12 18:38:40 -0800

    Fixed dynamic extension icon based on ToS page
    Extension now displays an "eyes open" icon when on a ToS page

--- extension/background.js
+++ extension/background.js
@@ -1,8 +1,21 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 "use strict";
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  // console.log('updated', id, changeInfo, tab);
+  chrome.tabs.get(activeInfo.tabId, function(tab) {
+    if (tab.url.includes("term")) {
+      chrome.pageAction.setIcon({
+        tabId: tab.id,
+        path: {
+          "16": "images/eyes16.png",
+          "32": "images/eyes32.png",
+          "48": "images/eyes48.png",
+          "128": "images/eyes128.png"
+        }
+      });
+    }
+  });
+});
 
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({ color: "#3aa757" }, function() {
